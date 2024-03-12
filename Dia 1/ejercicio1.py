ea8a46dd262f03fccd9cf3bc2f026fb3d51a86bb
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-03-12 12:27:16 -0300

    actualización de funciones y restricciones

---
+++ Dia 1/ejercicio1.py
@@ -0,0 +1,22 @@
+#comentario de una linea
+"""
+comentario multilinea
+mas de
+una linea
+"""
+print("Estamos en nuestro primer ejercicio de python")
+print(2 + 2)
+print(12 - 2)
+print(2 * 2)
+#print(2 / 0) -> Error no se puede dividir por 0
+print(100 / 99) #-> el resultado siempre sera un numero float
+
+num = 23
+print(num)
+
+# LIMITANTES
+# No esta permitido la suma de letras y numeros
+#print("texto"+12) Error no se pueden concatener numero y letras
+
+# INTERPOLACION
+print(f"el numero es {num}")
