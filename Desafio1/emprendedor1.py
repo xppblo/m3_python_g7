015d5111a324131dbd6e480fd8327ce04e18f59b
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-03-14 12:41:14 -0300

    fix de variables

--- Desafio1/emprendedor1.py
+++ Desafio1/emprendedor1.py
@@ -3,7 +3,7 @@
 print("Para calcular las utilidades necesitaremos que nos proporciones algunos datos.")
 
 precioSuscripcion = float(input("Ingresa el valor de las suscripcion:\n"))
-numeroUsuarios = float(input("Ingresa el número de usuarios:\n"))
+numeroUsuarios = int(input("Ingresa el número de usuarios:\n"))
 gastosTotales = float(input("Ingresa el valor de los gastos totales:\n"))
 
 utilidades = round(precioSuscripcion * numeroUsuarios - gastosTotales)
--- Desafio1/emprendedor3.py
+++ Desafio1/emprendedor3.py
@@ -1,7 +1,7 @@
 print("Para calcular las utilidades necesitaremos que nos proporciones algunos datos.")
 
 precioSuscripcion = float(input("Ingresa el valor de las suscripcion:\n"))
-numeroUsuarios = float(input("Ingresa el número de usuarios:\n"))
+numeroUsuarios = int(input("Ingresa el número de usuarios:\n"))
 gastosTotales = float(input("Ingresa el valor de los gastos totales:\n"))
 utilidadesAnterior = float(input("Ingrese las utilidades del año anterior:\n"))
 
