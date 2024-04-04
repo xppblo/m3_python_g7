d8f55bfb7071f4421cc681b8fc764bff4c663daa
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-04-04 10:43:01 -0300

    desafio API actualización

--- Desafio-API1/main.py
+++ Desafio-API1/main.py
@@ -1,8 +1,6 @@
-import api_get as user_data
-import api_post as created_user
-import api_put as updated_user
+import api_get
+import api_post
+import api_put 
 import api_delete 
 
-print(user_data)
-print(created_user)
-print(updated_user)
+print("Programa main donde se llaman todas las api's")
